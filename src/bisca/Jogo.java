b1815b15e1f291d11b0dec81bb4389efa50fbcb8
ghillary <119016108+ghillary@users.noreply.github.com>
2022-12-14 13:09:57 -0300

    Atualizando contagem

--- src/bisca/Jogo.java
+++ src/bisca/Jogo.java
@@ -14,6 +14,16 @@ int contarPontos(int[] mao) {
   // Inicializar variável para armazenar a contagem de pontos
   int pontos = 0;
 
+ // Percorrer cada carta da mão
+  for (int i = 0; i < mao.length; i++) {
+    // Obter o valor da carta
+    int valorCarta = mao[i];
+    
+    // Adicionar o valor da carta à contagem de pontos
+    // (a regra de contar pontos pode variar de acordo com a variante de bisca que está sendo jogada)
+    pontos += valorCarta;
+  }
+
     
 
 }
